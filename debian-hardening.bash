9172b0d15b6e95f6fede54667c1958fcd05a1bc3
Adiel de Lima Ribeiro <contato@nuvym.com>
2021-05-12 21:44:45 -0300

    Hardened by NUVYM

---
+++ debian-hardening.bash
@@ -0,0 +1,23 @@
+################# debian hardening #########################################################################
+#!/bin/bash 
+############################################################################################################
+######################### umask ############################################################################
+sudo bash -c "echo umask 077 >> /etc/profile"
+######################### networking #######################################################################
+sudo hostname master
+sudo bash -c "echo master > /etc/hostname"
+sudo bash -c "echo `/sbin/ifconfig | grep inet | awk {'print $2'} | awk 'NR==1'` `hostname`\
+>> /etc/cloud/templates/hosts.debian.tmpl"
+#############################################################################################################
+##################### ssh ###################################################################################
+sudo groupadd sshd
+sudo usermod -a -G sshd admin
+sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bkp
+sudo curl https://raw.githubusercontent.com/Adiel-Ribeiro/Debian/master/sshd_config -o /etc/ssh/sshd_config
+sudo chmod 400 /etc/ssh/sshd_config 
+#############################################################################################################
+##################### logging ###############################################################################
+sudo chattr +a /var/log/auth.log
+#############################################################################################################
+rm debian-hardening.bash
+sudo reboot
